c1d3bf14bf4ac46e6103f8fd4cda745bece41573
xinz <joy2everyone.z@gmail.com>
2020-11-18 11:43:52 +0800

    Version 1.1.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExAliyun.MNS.MixProject do
     [
       app: :ex_aliyun_mns,
       name: "ExAliyun.MNS",
-      version: "1.0.3",
+      version: "1.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
