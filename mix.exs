9944e012e78e2a42a497fcbe7e4e0acf3d11a822
xinz <joy2everyone.z@gmail.com>
2021-01-15 10:02:43 +0800

    Fix Elixir 1.11 compilation warnings

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule ExAliyun.MNS.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :eex],
       mod: {ExAliyun.MNS.Application, []}
     ]
   end
